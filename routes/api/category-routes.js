0f8ea6b2128e9a524fdc2cd952512e4f4e8bf682
Sishir <sishir.bhattarai@outlook.com>
2020-12-13 14:46:10 -0700

    added code on category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,15 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const findAllCategories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(findAllCategories);
+     }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
