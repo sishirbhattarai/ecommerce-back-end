2343c2384796872ffaa6b24ae9a02d6bc1f4f364
Sishir <sishir.bhattarai@outlook.com>
2020-12-14 23:09:17 -0700

    added code on tag route on post method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,6 +38,18 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then(function(newTag) {
+    res.json(newTag);
+  })
+   .catch(function(err) {
+     res.json(err)
+   });
+//   .catch((err)=> {
+//     res.json(err);
+// })
 });
 
 router.put('/:id', (req, res) => {
