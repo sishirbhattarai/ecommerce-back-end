c92c6b36021082318b3aa523aff45b44696d79b5
Sishir <sishir.bhattarai@outlook.com>
2020-12-15 23:00:03 -0700

    code added on product route and tested sucessfully

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,11 +41,12 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
+    
   */
   Product.create(req.body)
     .then((product) => {
